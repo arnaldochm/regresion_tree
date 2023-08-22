0bead9f7c0ee093ebb9fdaffabbdbb1143ee733d
arnaldochm <arnaldochacon1996@gmail.com>
2023-08-22 00:53:58 +0000

    ...

---
+++ installation.sh
@@ -0,0 +1,10 @@
+echo "Starting Installation"
+python -m venv .venv
+. .venv/bin/activate
+pip install pandas matplotlib scikit-learn ipykernel seaborn
+pip freeze > requirements.txt
+mkdir -p data/raw
+mkdir -p data/processed
+mkdir -p data/final
+mkdir notebooks
+echo "Finished"
